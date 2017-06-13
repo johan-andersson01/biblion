5ae1127e6432fd3ba3a36093109dd013e50aa700
Johan <johan.andersson95@gmail.com>
2017-06-13 16:16:17 +0200

    Added user profile page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,11 @@
 Rails.application.routes.draw do
   root 'pages#home'
-
-  get 'signup' => 'pages#signup'
+  get 'signup', to: 'pages#signup'
+  get 'contact', to: 'pages#contact'
+  get 'about', to: 'pages#about'
 
   get 'users/new'
 
+  resources :users
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
