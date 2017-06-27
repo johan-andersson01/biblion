da9a8641e4eef043744dcea9c6b6f90269b94588
Johan <johan.andersson95@gmail.com>
2017-06-27 21:21:28 +0200

    Allow landscape to be blank

--- app/models/user.rb
+++ app/models/user.rb
@@ -4,7 +4,7 @@ class User < ApplicationRecord
   before_save { email.downcase!}
   validates :name, presence: true, length: {minimum: 2, maximum: 50}
   validates :location, presence: true, length: {minimum: 2}
-  validates :landscape, presence: true
+  # validates :landscape, presence: true
   VALID_EMAIL = /\A[\w+\-.]+@[a-z\d\-]+(\.[a-z\d\-]+)*\.[a-z]+\z/i
   VALID_TEL = /\A[\d]+\z/i #/\Ad{8,11}\z
   validates :email, presence: true, length: {minimum: 5, maximum: 255},
