8afacd7f607c02fa654b4cdae2eb8dcaf12e1287
Johan <johan.andersson95@gmail.com>
2017-10-29 01:33:54 +0200

    add length limits to book

--- app/models/book.rb
+++ app/models/book.rb
@@ -3,11 +3,11 @@ class Book < ApplicationRecord
   default_scope -> { order(created_at: :desc) }
   attr_accessor :query
   validates :user_id, presence: true
-  validates :author, presence: true
-  validates :title, presence: true
-  validates :quality, presence: true
-  validates :genre, presence: true
-  validates :language, presence: true
+  validates :author, presence: true, length: {minimum: 4, maximum: 100}
+  validates :title, presence: true, length: {minimum: 2, maximum 150}
+  validates :quality, presence: true, length: {maximum: 10}
+  validates :genre, presence: true, length: {maximum: 30}
+  validates :language, presence: true, length: {maximum: 30}
 
   
   def self.search(query)
