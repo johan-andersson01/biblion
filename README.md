25d98270791570c7407ff2021f2c1407c32c7afb
Johan <johan.andersson95@gmail.com>
2017-10-29 02:13:12 +0200

    Update readme

--- README.md
+++ README.md
@@ -5,7 +5,7 @@ application up and running.
 
 Things you may want to cover:
 
-* Ruby version
+* Developed with Ruby 2.4
 
 * System dependencies
 
@@ -22,3 +22,36 @@ Things you may want to cover:
 * Deployment instructions
 
 * ...
+
+# Requirements
+
+* Ruby >= 2.4
+* Rails >= 5.0
+
+# Local setup
+` git clone https://github.com/johan-andersson01/biblion.git`
+
+` cd /your_path/biblion`
+
+` bundle install `
+
+` rails db:migrate`
+
+` rails db:seed`
+
+` rails s `
+
+After executing these commands, the server will be up and running at localhost:3000.
+
+You will have created one admin user with the `db:seed` command, with the following credentials:
+
+> email: admin@foo.bar
+
+> password: foobar
+
+# Porting to another language
+
+Since this project has been built for Swedish users in mind, much of the interface is written in Swedish. Here are some tips if you want to port this project to another language & location:
+
+* Go through all files under `/app/views/` and look for Swedish words and sentences.
+* Replace all instances of `CS.states(:se)` with your country of choice.
--- db/seeds.rb
+++ db/seeds.rb
@@ -1,36 +1,9 @@
-User.create!(name:  "Johan",
-             email: "johan@biblion.se",
+User.create!(name:  "admin",
+             email: "admin@foo.bar",
              password:              "foobar",
              password_confirmation: "foobar",
              admin: true,
              activated: true,
              activated_at: Time.zone.now,
-             location: Faker::HarryPotter.location,
-             landscape: Faker::StarWars.planet)
-
-2.times do |n|
-  name  = Faker::Name.name.first
-  email = "user-#{n+1}@biblion.se"
-  password = "password"
-  User.create!(name:  name,
-               email: email,
-               password: password,
-               password_confirmation: password,
-               activated: true,
-               activated_at: Time.zone.now,
-               location: Faker::HarryPotter.location,
-               landscape: Faker::StarWars.planet)
-end
-#
-# users = User.order(:created_at).take(10)
-# 60.times do
-#   title = Faker::Zelda.character
-#   author = Faker::Name.name
-#   year = Faker::Date.between(100.years.ago, Date.today)
-#   description = Faker::Lorem.sentence(15)
-#   swaps = Faker::Number.between(0, 10)
-#   available = [true, false].sample
-#   cover = Faker::LoremPixel.image("150x224")
-#   users.each { |user| user.books.create!(title: title, author: author, year: year,
-#     description: description, swaps: swaps, available: available, cover: cover) }
-# end
+             location: "Admin City",
+             landscape: "Admin County ")
