31fcfbfb67e8fcb9c68b15df68b2366cccaf9955
Johan <johan-andersson01@protonmail.com>
2017-12-17 01:32:08 +0100

    Add test user in seed, add meta html info

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,3 +7,12 @@ User.create!(name:  "admin",
              activated_at: Time.zone.now,
              location: "Admin City",
              landscape: "Admin County ")
+User.create!(name:  "tester",
+             email: "tester@foo.bar",
+             password:              "foobar9000",
+             password_confirmation: "foobar9000",
+             admin: false,
+             activated: true,
+             activated_at: Time.zone.now,
+             location: "Test City",
+             landscape: "Test County ")
