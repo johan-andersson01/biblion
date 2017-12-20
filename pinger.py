b65b2918e484d93ca2007d4167043f23b20b2655
Johan <johan-andersson01@protonmail.com>
2017-12-20 23:04:56 +0100

    Pinger: use python2 instead

--- pinger.py
+++ pinger.py
@@ -1,10 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/python2
 import requests
 import datetime
 import time
 
 while True:
-    hour = datetime.datetime.now().hour
-    if hour > 7:
+    if datetime.datetime.now().hour > 7:
         requests.get('https://biblion.se')
     time.sleep(60*29)
